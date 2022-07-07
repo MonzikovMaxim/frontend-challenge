e87cf41023730198db41c65e01499fdf6b56a61f
Monzikov Maxim <monzikovm@yandex.ru>
2022-07-07 13:26:10 +0300

    feat: version 5

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import "./App.scss";
-import {  Routes, Route } from "react-router-dom";
+import {BrowserRouter,  Routes, Route } from "react-router-dom";
 import Main from "./components/Main/Main";
 import SavedCards from "./components/SavedCards/SavedCards";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <div className="app">
           <Navbar />
           <Routes>
-            <Route path="/" element={<Main />}></Route>
+            <Route path="/frontend-challenge" element={<Main />}></Route>
             <Route path="/likes" element={<SavedCards />} />
           </Routes>
         </div>
-    </>
+    </BrowserRouter>
   );
 }
 
