ea245200f189b8a518a317193f4a6843f56eca08
Monzikov Maxim <monzikovm@yandex.ru>
2022-07-07 13:16:54 +0300

    feat: version 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import Main from "./components/Main/Main";
 import SavedCards from "./components/SavedCards/SavedCards";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <div className="app">
           <Navbar />
           <Routes>
@@ -14,7 +14,7 @@ function App() {
             <Route path="/likes" element={<SavedCards />} />
           </Routes>
         </div>
-    </BrowserRouter>
+    </>
   );
 }
 
